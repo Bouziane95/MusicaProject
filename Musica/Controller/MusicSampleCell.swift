7595a72ebbd112d3a50049c838cebc678a2772ec
Bouziane <bey.bouziane@live.fr>
2019-12-18 13:18:33 +0100

    TableView Added to manage different sample of music

---
+++ Musica/Controller/MusicSampleCell.swift
@@ -0,0 +1,14 @@
+//
+//  MusicSampleCell.swift
+//  Musica
+//
+//  Created by Bouziane Bey on 18/12/2019.
+//  Copyright © 2019 Bouziane Bey. All rights reserved.
+//
+
+import UIKit
+
+class MusicSampleCell: UITableViewCell {
+
+    @IBOutlet weak var titleMusicCell: UILabel!
+}
